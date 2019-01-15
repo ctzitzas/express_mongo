be3ab93cfd337363cfac0b3b11c581bd1747cb5c
Chris Tzitzas <ctzitzas@gmail.com>
2019-01-15 16:03:58 +1100

    Create session, profile route and profile template

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-// var session = require('express-session');
+var session = require('express-session');
 var app = express();
 
 // use sessions for tracking logins
 
-// app.use(session({
-
-// }));
+app.use(session({
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false
+}));
 
 // mongodb connection
 mongoose.connect('mongodb://localhost:27017/bookworm');
@@ -18,7 +20,9 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // serve static files from /public
 app.use(express.static(__dirname + '/public'));
@@ -32,7 +36,7 @@ var routes = require('./routes/index');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
@@ -40,7 +44,7 @@ app.use(function(req, res, next) {
 
 // error handler
 // define as the last app.use callback
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
